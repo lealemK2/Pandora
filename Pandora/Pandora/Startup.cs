310ad817005f69b3e0e21d56d2a1247bc213af37
Lealemk2 <Lealem.kiflelklklk2@gmail.com>
2021-12-27 11:15:34 +0300

    First Pandora commit

---
+++ Pandora/Pandora/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pandora.Startup))]
+namespace Pandora
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
