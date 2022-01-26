c7182a45948a42e8f78d2bbe3a62858323cb590a
Lealemk2 <Lealem.kiflelklklk2@gmail.com>
2022-01-26 15:32:33 +0300

    finished_30

--- Pandora/Pandora/Controllers/CustomersController.cs
+++ Pandora/Pandora/Controllers/CustomersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Pandora.Controllers
 {
@@ -23,7 +24,7 @@ namespace Pandora.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _context.Customers.ToList();
+            var customers = _context.Customers.Include(c=>c.MembershipType).ToList();
             return View(customers);
         }
 
